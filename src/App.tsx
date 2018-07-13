f62fa7c35922c3bf9d3aa6a8d501b35a68044e74
Richard Lee <rjdlee@protonmail.com>
2018-07-13 00:41:13 -0400

    Fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -53,9 +53,10 @@ class App extends React.Component<HasMarketsSummary, State> {
             </div>
             <div className="column is-12-mobile is-5-tablet is-5-desktop has-text-centered content">
               <p><strong>See What the World Thinks.</strong></p>
-              <p>Prediction Markets powered by Augur. Each market trades on the <a href="https://augur.net"
-                                                                                   target="blank">Augur</a>
-                decentralized prediction market platform, built on the Ethereum blockchain.</p>
+              <p>
+                Prediction Markets powered by Augur. Each market trades on the <a href="https://augur.net" target="blank">Augur</a>
+                {' decentralized prediction market platform, built on the Ethereum blockchain.'}
+              </p>
             </div>
           </div>
         </section>
@@ -81,7 +82,7 @@ const sortOrders: Map<string, (a: Market, b: Market) => number> = new Map([
       return 1;
     }
 
-    return aCapitalization.getUsd() - bCapitalization.getUsd()
+    return bCapitalization.getUsd() - aCapitalization.getUsd()
   }],
   ['New Markets', (a: Market, b: Market) => b.getCreationTime() - a.getCreationTime()],
   ['Ending Soon', (a: Market, b: Market) => a.getEndDate() - b.getEndDate()]
@@ -135,19 +136,25 @@ class MarketList extends React.Component<MarketListProps, MarketListState> {
     const {marketList, currencySelectionObserver} = this.props;
     const {paginationLimit, paginationLimitOwner, paginationOffset, showEnded, sortOrder, sortOrderOwner, searchQuery, category, categoryOwner} = this.state;
 
+    // Possible to optimize by only updating when props received
     const categories = ['All'].concat(Array.from(marketList.reduce((acc: Set<string>, market: Market) => acc.add(market.getCategory()), new Set()).values()));
 
     const filteredMarketList = marketList
       .filter((market: Market) => category === 'All' || market.getCategory() === category)
       .filter((market: Market) => showEnded || moment.unix(market.getEndDate()).isAfter())
+
+      // Basic fuzzy text search
       .filter((market: Market) => market.getName().toLowerCase().replace(' ', '').indexOf(searchQuery) !== -1)
       .sort(sortOrders.get(sortOrder));
 
     const numberOfPages = Math.ceil(filteredMarketList.length / paginationLimit);
+
+    // Index may be out of bounds if user changes query parameter
     const paginationIndex = Math.min(Math.max(paginationOffset, 0), numberOfPages - 1);
     const paginationStart = paginationLimit * paginationIndex;
     const paginationEnd = paginationLimit * (paginationIndex + 1);
 
+    // Show the following pages unless they overlap: first, previous, current, next, last
     const paginationIndices = Array.from(new Set([
       1,
       Math.max(paginationIndex, 1),
@@ -156,43 +163,52 @@ class MarketList extends React.Component<MarketListProps, MarketListState> {
       numberOfPages]
     ).values());
 
+    // Negative page indices represents ellipses
+    if(paginationIndex > 1) {
+      paginationIndices.splice(1, 0, -1);
+    }
+
+    if(paginationIndex < numberOfPages - 2) {
+      paginationIndices.splice(paginationIndices.length - 1, 0, -2);
+    }
+
     const now = moment();
 
     return (
       <section className="less-padding-top section">
-        <div
-          className="column is-paddingless has-text-centered-mobile has-text-left-tablet has-text-left-desktop">
-          <p><strong>Prediction Markets</strong></p>
-        </div>
         <div className="columns is-centered is-vcentered is-mobile is-multiline">
-          <div className="column">
+          <div
+            className="column is-paddingless has-text-centered-mobile has-text-left-tablet has-text-left-desktop">
+            <p><strong>Prediction Markets</strong></p>
+          </div>
+          <div className="column is-narrow">
             <Selector
               currentValueObserver={currencySelectionObserver}
               renderValue={renderCurrency}
               setValue={this.setCurrency}
               values={[Currency.USD, Currency.ETH, Currency.BTC]}/>
           </div>
-          <div className="column">
+          <div className="column is-narrow">
             <Selector
               currentValueObserver={sortOrderOwner.o}
               renderValue={this.renderSortOrder}
               setValue={this.setSortOrder}
               values={Array.from(sortOrders.keys())}/>
           </div>
-          <div className="column">
+          <div className="column is-narrow">
             <Selector
               currentValueObserver={categoryOwner.o}
               renderValue={this.renderCategory}
               setValue={this.setCategory}
               values={categories}/>
           </div>
-          <div className="column">
+          <div className="column is-narrow">
             <label className="checkbox">
               <input type="checkbox" onChange={this.setShowEnded}/>&nbsp;
               Show Ended Markets
             </label>
           </div>
-          <div className="column">
+          <div className="column is-narrow">
             <div className="search">
               <input className="input" type="text" placeholder="Search" onChange={this.setSearchQuery}/>
               <i className="fas fa-search"/>
@@ -210,35 +226,53 @@ class MarketList extends React.Component<MarketListProps, MarketListState> {
                 m={m}
                 index={paginationStart + index}/>)
             )}
-          <nav className="pagination is-centered" role="navigation" aria-label="pagination">
+        <div className="columns is-vcentered">
+          <div className="column is-narrow is-paddingless">
+            <label>
+              Markets Per Page
+            </label>
+          </div>
+          <div className="column is-narrow">
             <Selector
               currentValueObserver={paginationLimitOwner.o}
               renderValue={this.renderPaginationLimit}
               setValue={this.setPaginationLimit}
               values={paginationLimits}
             />
-            {paginationIndex > 0 && (
-              <a className="pagination-previous" onClick={this.setPaginationOffset.bind(this, paginationIndex - 1)}>Prev
-                Page</a>)}
-            {paginationIndex < numberOfPages - 1 && (
-              <a className="pagination-next" onClick={this.setPaginationOffset.bind(this, paginationIndex + 1)}>Next
-                Page</a>)}
-            <ul className="pagination-list">
-              {paginationIndices.map((page: number) => (
-                <li key={page}>
-                  <a
-                    className={classNames('pagination-link', page === (paginationIndex + 1) && 'is-current')}
-                    aria-label={`Goto page ${page}`}
-                    aria-current="page"
-                    onClick={this.setPaginationOffset.bind(this, page - 1)}
-                  >
-                    {page}
-                  </a>
-                </li>
-              ))
-              }
-            </ul>
-          </nav>
+          </div>
+          <div className="column">
+            <nav className="pagination" role="navigation" aria-label="pagination">
+              <label>
+                &nbsp;&nbsp;Page&nbsp;
+              </label>
+              {paginationIndex > 0 && (
+                <a className="pagination-previous" onClick={this.setPaginationOffset.bind(this, paginationIndex - 1)}>Prev
+                  Page</a>)}
+              {paginationIndex < numberOfPages - 1 && (
+                <a className="pagination-next" onClick={this.setPaginationOffset.bind(this, paginationIndex + 1)}>Next
+                  Page</a>)}
+              <ul className="pagination-list">
+                { paginationIndices.map((page: number) => page < 0 ? (
+                  <li key={page}>
+                    <span className="pagination-ellipsis">&hellip;</span>
+                  </li>
+                ) :(
+                  <li key={page}>
+                    <a
+                      className={classNames('pagination-link', page === (paginationIndex + 1) && 'is-current')}
+                      aria-label={`Goto page ${page}`}
+                      aria-current="page"
+                      onClick={this.setPaginationOffset.bind(this, page - 1)}
+                    >
+                      {page}
+                    </a>
+                  </li>
+                ))
+                }
+              </ul>
+            </nav>
+          </div>
+        </div>
         </div>
       </section>
     );

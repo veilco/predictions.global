47a3c5774384c70e35fb07bebd8a53a90325c38b
Ryan Berckmans <ryanberckmans@gmail.com>
2018-07-29 10:49:55 -0400

    Add new public ethereum node

--- src/PublicEthereumNodes.tsx
+++ src/PublicEthereumNodes.tsx
@@ -47,6 +47,11 @@ const PublicEthereumNodes: React.SFC<Props> = (props) => {
                   <td><code>https://ponytail.io/</code></td>
                   <td><code>wss://ponytail.io/ws</code></td>
                 </tr>
+                <tr>
+                  <td><strong>#4</strong></td>
+                  <td><code>https://epoxide.io/</code></td>
+                  <td><code>wss://epoxide.io/ws</code></td>
+                </tr>
               </tbody>
             </table>
           </div>

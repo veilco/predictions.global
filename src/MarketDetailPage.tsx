a4aecb79fd4bcb3dbc144642fedea844ac9ad88b
Ryan Berckmans <ryanberckmans@gmail.com>
2018-07-30 19:07:15 -0400

    Use only MarketInfo.details instead of Market.details which is deprecated

--- src/MarketDetailPage.tsx
+++ src/MarketDetailPage.tsx
@@ -142,7 +142,7 @@ export class MarketDetailPage extends React.Component<Props, State> {
     const openInterest = m.getMarketCapitalization();
     const prediction = renderPrediction(mt, m.getPredictionsList());
     const marketSummary = getMarketSummaryString(name, openInterest, prediction);
-    const details = m.getDetails().trim();
+    const details = mi.getDetails().trim();
     // otherDetails is rendered in two places; one for each of mobile and desktop.
     const otherDetails = <div className="box">
       <div className="columns is-vcentered is-multiline">
@@ -228,7 +228,7 @@ export class MarketDetailPage extends React.Component<Props, State> {
       {Footer}
       <Helmet>
         <title>Odds on {m.getName().substring(0, 85)}</title>
-        <meta name="description" content={`Augur prediction market data and statistics. ${m.getDetails().substring(0, 250)}`} />
+        <meta name="description" content={`Augur prediction market data and statistics. ${details.substring(0, 250)}`} />
       </Helmet>
     </div>;
   }

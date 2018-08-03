bc535a2f97e78c510cefe2aa8252c33719756b37
Ryan Berckmans <ryanberckmans@gmail.com>
2018-08-03 15:51:56 -0400

    Remove list of public augur nodes

--- src/PublicEthereumNodes.tsx
+++ src/PublicEthereumNodes.tsx
@@ -13,7 +13,7 @@ const PublicEthereumNodes: React.SFC<Props> = (props) => {
   return <div>
     <Header ms={props.ms} currencySelectionObserver={props.currencySelectionObserver} doesClickingLogoReloadPage={false} headerContent={
       <div className="has-text-centered content">
-        <h3 className="title">Public Ethereum &amp; Augur Nodes</h3>
+        <h3 className="title">Public Ethereum Nodes</h3>
         <p>These nodes can be used as the back end for <a href="https://github.com/AugurProject/augur-app/releases" target="_blank">Augur App</a>. (Eg. as an alternative to Infura.)</p>
         <p>This list is provided by Predictions.Global for your convenience. We trust the community members hosting these nodes, but make no security guarantees.</p>
       </div>
@@ -55,28 +55,12 @@ const PublicEthereumNodes: React.SFC<Props> = (props) => {
               </tbody>
             </table>
           </div>
-          <div className="column is-narrow">
-            <table>
-              <thead>
-                <tr>
-                  <th />
-                  <th>Augur Node Websocket Endpoint</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><strong>#1</strong></td>
-                  <td><code>wss://augur.gethstar.com</code></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
         </div>
       </div>
     </section>
     {Footer}
     <Helmet>
-      <title>List of public Ethereum and Augur Node servers with HTTP and Websocket endpoints for augur-app | Predictions.Global</title>
+      <title>List of public Ethereum Node servers with HTTP and Websocket endpoints for Augur App | Predictions.Global</title>
       <meta name="description" content="List of community-curated, trusted public Ethereum and Augur Node servers for use with Augur App. Endpoints are available for HTTP and Websocket connections. This may be useful as an alternative to Infura." />
     </Helmet>
   </div>;
